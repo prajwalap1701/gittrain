7d56eac7187d0db679184fcdb7389fcd1dee17ac
Prajwal <prajwalap1701@gmail.com>
2023-09-21 19:06:51 +0530

    Add ci.yml

---
+++ .github/bin/script.sh
@@ -0,0 +1,4 @@
+# You can access the defined environment variables here as well
+echo "Environment Variable 1: $ENV_VARIABLE_1"
+echo "Environment Variable 2: $ENV_VARIABLE_2"
+# Add more commands as needed
