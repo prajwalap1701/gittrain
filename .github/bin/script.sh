339f428c2519f7a1f23c894e67baec5ca62a49a4
Prajwal <prajwalap1701@gmail.com>
2023-09-21 19:12:15 +0530

    d

--- .github/bin/script.sh
+++ .github/bin/script.sh
@@ -1,4 +1,4 @@
 # You can access the defined environment variables here as well
-echo "Environment Variable 1: $ENV_VARIABLE_1"
-echo "Environment Variable 2: $ENV_VARIABLE_2"
+echo "Environment Variable 1 from script: $ENV_VARIABLE_1"
+echo "Environment Variable 2 from script: $ENV_VARIABLE_2"
 # Add more commands as needed
